21adc411702535ab625590cd95d6819afa560882
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-05-26 08:14:29 -0300

    add swagger app use

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from 'express'
-import swagger from 'swagger-ui-express'
+import swaggerUI from 'swagger-ui-express'
 import fs from 'fs'
+import path from 'path'
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
+const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname, '../docs', 'pets-swagger.json'), 'utf8'))
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to the API documentation server')
+    res.send('Welcome to the API documentation server. Visit /api-docs for the Swagger documentation.')
 })
 
 app.listen(PORT, () => {
